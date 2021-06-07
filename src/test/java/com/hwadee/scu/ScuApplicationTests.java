cd65e06398e65a97e4864324a7c48e7fb69108be
fanyang <1763089574@qq.com>
2021-06-07 11:48:46 +0800

    2021-7-7 11:48 项目框架搭建

---
+++ src/test/java/com/hwadee/scu/ScuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwadee.scu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
