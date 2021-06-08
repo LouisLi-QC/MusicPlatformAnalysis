5fe2ce28d2e6af17220dd5992771b96633c646f9
fanyang <1763089574@qq.com>
2021-06-08 09:57:23 +0800

    2021-6-8 9：57 修改pom文件解决mvc视图问题

---
+++ src/main/java/com/hwadee/scu/controller/RouterController.java
@@ -0,0 +1,51 @@
+package com.hwadee.scu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class RouterController {
+    @RequestMapping("/EnterToDashBoardv2")
+    public String EnterToWelcome(){
+        return "calendar";
+    }
+    @RequestMapping("/EnterToTalentQuery")
+    public String EnterToTalentQuery(HttpServletRequest request){
+        return "TalentQuery";
+    }
+    @RequestMapping("/EnterToTalentCompare")
+    public String EnterToTalentCompare(){
+        return "TalentCompare";
+    }
+    @RequestMapping("/EnterToQueryStaff")
+    public String EnterToQueryStaff(HttpServletRequest request){
+        return "QueryStaff";
+    }
+    @RequestMapping("/EnterToAuditStaff")
+    public String EnterToAuditStaff(){
+        return  "AuditStaff";
+    }
+
+    @RequestMapping("/EnterToAddEvaluation")
+    public String EnterToAddEvaluation(){
+        return "AddEvaluation";
+    }
+
+    @RequestMapping("/EnterToCompanyInformation")
+    public String EnterToCompanyInformation(){
+        return "CompanyInformationOperation";
+    }
+    @RequestMapping("/EnterToCompanyCertification")
+    public String EnterToCompanyCertification(){
+        return "CompanyCertification";
+    }
+    @RequestMapping("/EnterToMission")
+    public String EnterToMission(){
+        return "Mission";
+    }
+
+}
